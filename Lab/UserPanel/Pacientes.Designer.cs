5c40a155d949dbda37c3d9e283a72809e71546f3
Manuel Viscarra <36599299+Aviscad@users.noreply.github.com>
2021-04-04 10:02:58 -0600

    pacientes

--- Lab/UserPanel/Pacientes.Designer.cs
+++ Lab/UserPanel/Pacientes.Designer.cs
@@ -29,19 +29,43 @@ namespace Lab.UserPanel
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtCodigo = new System.Windows.Forms.TextBox();
-            this.txtEdad = new System.Windows.Forms.TextBox();
-            this.cbbCampania = new System.Windows.Forms.ComboBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
             this.cbbGenero = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.cbbCampania = new System.Windows.Forms.ComboBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGuardar);
             this.groupBox1.Controls.Add(this.cbbGenero);
             this.groupBox1.Controls.Add(this.cbbCampania);
             this.groupBox1.Controls.Add(this.txtEdad);
@@ -49,40 +73,84 @@ namespace Lab.UserPanel
             this.groupBox1.Controls.Add(this.txtNombre);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(512, 225);
+            this.groupBox1.Size = new System.Drawing.Size(471, 199);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // txtNombre
+            // btnEliminar
             // 
-            this.txtNombre.Location = new System.Drawing.Point(211, 39);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(121, 20);
-            this.txtNombre.TabIndex = 0;
+            this.btnEliminar.Location = new System.Drawing.Point(367, 115);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
-            // txtCodigo
+            // btnCancelar
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(211, 65);
-            this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(121, 20);
-            this.txtCodigo.TabIndex = 1;
+            this.btnCancelar.Location = new System.Drawing.Point(367, 68);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
             // 
-            // txtEdad
+            // label5
             // 
-            this.txtEdad.Location = new System.Drawing.Point(211, 91);
-            this.txtEdad.Name = "txtEdad";
-            this.txtEdad.Size = new System.Drawing.Size(121, 20);
-            this.txtEdad.TabIndex = 2;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "edad";
             // 
-            // cbbCampania
+            // label4
             // 
-            this.cbbCampania.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbbCampania.FormattingEnabled = true;
-            this.cbbCampania.Location = new System.Drawing.Point(211, 117);
-            this.cbbCampania.Name = "cbbCampania";
-            this.cbbCampania.Size = new System.Drawing.Size(121, 21);
-            this.cbbCampania.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "codigo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "genero";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "campa;a";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(367, 39);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button1_Click);
             // 
             // cbbGenero
             // 
@@ -96,27 +164,130 @@ namespace Lab.UserPanel
             this.cbbGenero.Size = new System.Drawing.Size(121, 21);
             this.cbbGenero.TabIndex = 4;
             // 
-            // button1
+            // cbbCampania
             // 
-            this.button1.Location = new System.Drawing.Point(367, 39);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cbbCampania.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbCampania.FormattingEnabled = true;
+            this.cbbCampania.Location = new System.Drawing.Point(211, 117);
+            this.cbbCampania.Name = "cbbCampania";
+            this.cbbCampania.Size = new System.Drawing.Size(121, 21);
+            this.cbbCampania.TabIndex = 3;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(211, 91);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(121, 20);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(211, 65);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(211, 39);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.AllowUserToAddRows = false;
+            this.dgvPacientes.AllowUserToDeleteRows = false;
+            this.dgvPacientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPacientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPacientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column6,
+            this.Column5});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPacientes.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvPacientes.Location = new System.Drawing.Point(12, 217);
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.ReadOnly = true;
+            this.dgvPacientes.RowHeadersWidth = 25;
+            this.dgvPacientes.RowTemplate.Height = 30;
+            this.dgvPacientes.Size = new System.Drawing.Size(959, 326);
+            this.dgvPacientes.TabIndex = 1;
+            this.dgvPacientes.DoubleClick += new System.EventHandler(this.dgvPacientes_DoubleClick);
+            // 
+            // Column5
+            // 
+            this.Column5.FillWeight = 102.994F;
+            this.Column5.HeaderText = "Genero";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Codigo";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 102.994F;
+            this.Column4.HeaderText = "Campaña";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 115.2801F;
+            this.Column3.HeaderText = "Edad";
+            this.Column3.MinimumWidth = 50;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 115.2801F;
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 63.45177F;
+            this.Column1.HeaderText = "Id Paciente";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
             // 
             // Pacientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 621);
+            this.ClientSize = new System.Drawing.Size(980, 553);
+            this.Controls.Add(this.dgvPacientes);
             this.Controls.Add(this.groupBox1);
             this.Name = "Pacientes";
             this.Text = "Pacientes";
             this.Load += new System.EventHandler(this.Pacientes_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -129,6 +300,20 @@ namespace Lab.UserPanel
         private System.Windows.Forms.TextBox txtEdad;
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvPacientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
--- Lab/UserPanel/Pacientes.cs
+++ Lab/UserPanel/Pacientes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,26 +18,132 @@ namespace Lab.UserPanel
             InitializeComponent();
         }
 
+
+        private void Clear()
+        {
+            txtNombre.Text = txtEdad.Text = txtCodigo.Text = "";
+            btnEliminar.Enabled = false;
+            btnGuardar.Text = "Guardar";
+            pacienteModel.id_paciente = 0;
+        }
+
+        paciente pacienteModel = new paciente();
+
         private void Pacientes_Load(object sender, EventArgs e)
         {
+            //Fill campaña
             using (laboratorio_pEntities DB = new laboratorio_pEntities()) 
             {
                 var query = from campaña in DB.campaña select campaña;
                 var campaniaTolist = query.ToList();
-
                 cbbCampania.DataSource = campaniaTolist;
                 cbbCampania.DisplayMember = "nombre";
                 cbbCampania.ValueMember = "id_campaña";
-
-                cbbGenero.SelectedIndex = 0;
-                           
+                cbbGenero.SelectedIndex = 0;     
             }
+
+            fillRows();
+            Clear();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //ID de campa;a seleccionada del combobox
-            MessageBox.Show(cbbCampania.SelectedValue.ToString());   
+            if (txtNombre.Text.Trim().Equals("") || txtEdad.Text.Trim().Equals("") || txtEdad.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("Rellene todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                pacienteModel.nombre = txtNombre.Text.Trim();
+                pacienteModel.edad =  Convert.ToInt32(txtEdad.Text.Trim());
+                pacienteModel.codigo = txtCodigo.Text.Trim();
+                pacienteModel.genero = cbbGenero.SelectedItem.ToString().Trim();
+                pacienteModel.id_campaña = Convert.ToInt32(cbbCampania.SelectedValue.ToString().Trim());
+
+                using (laboratorio_pEntities DB = new laboratorio_pEntities())
+                {
+                    if (pacienteModel.id_paciente == 0)
+                    {
+                        DB.paciente.Add(pacienteModel);
+                        MessageBox.Show("Registro agregado", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        DB.Entry(pacienteModel).State = EntityState.Modified;
+                        MessageBox.Show("Registro actualizado", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    DB.SaveChanges();
+                }
+                fillRows();
+                Clear();
+            }
+        }
+        private void fillRows() {
+            dgvPacientes.Rows.Clear();
+            using (laboratorio_pEntities DB = new laboratorio_pEntities())
+            {
+
+                foreach (var paciente in DB.paciente)
+                {
+                    var query = from campaña in DB.campaña where campaña.id_campaña == paciente.id_campaña select campaña;
+                    var getCampania = query.ToList();
+
+                    dgvPacientes.Rows.Add(paciente.id_paciente, paciente.nombre, paciente.edad, getCampania[0].nombre, paciente.codigo, paciente.genero);
+                }
+            }
+        }
+
+        private void dgvPacientes_DoubleClick(object sender, EventArgs e)
+        {
+            pacienteModel.id_paciente = Convert.ToInt32(dgvPacientes.CurrentRow.Cells[0].Value);
+
+            using (laboratorio_pEntities DB = new laboratorio_pEntities())
+            {
+                try
+                {
+                    pacienteModel = DB.paciente.Where(x => x.id_paciente == pacienteModel.id_paciente).FirstOrDefault();
+                    
+                    txtNombre.Text = pacienteModel.nombre;
+                    txtEdad.Text = pacienteModel.edad.ToString();
+                    txtCodigo.Text = pacienteModel.codigo;
+
+                    cbbCampania.SelectedValue = pacienteModel.id_campaña;
+                    if (pacienteModel.genero.Equals("Masculino	"))
+                    {
+                        cbbGenero.SelectedIndex = 0;
+                    }
+                    else if(pacienteModel.genero.Equals("Femenino")){
+                        cbbGenero.SelectedIndex = 1;
+                    }
+                    
+                    btnGuardar.Text = "Modificar";
+                    btnEliminar.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Estas seguro de eliminar este registro, luego de hacerlo no se podra recuperar la información?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                using (laboratorio_pEntities DB = new laboratorio_pEntities())
+                {
+                    var entry = DB.Entry(pacienteModel);
+                    if (entry.State == EntityState.Detached)
+                        DB.paciente.Attach(pacienteModel);
+                    DB.paciente.Remove(pacienteModel);
+                    DB.SaveChanges();
+
+                    Clear();
+                    fillRows();
+
+                    MessageBox.Show("Registro eliminado", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
